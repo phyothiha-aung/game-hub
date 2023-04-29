fedcd24653cd1e5dce6bbd3d390686dc8cfaf5d0
phyothiha-aung <phyothiha050@gmail.com>
2023-04-29 15:42:51 +0630

    refector: GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,32 +12,31 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  if (error) return <Text colorScheme="red">{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 4,
-        }}
-        spacing={5}
-        padding={{
-          sm: 2,
-          md: 4,
-        }}
-        paddingLeft={{
-          lg: 0,
-        }}
-        justifyContent="center"
-      >
-        {isLoading && skeletons.map((s) => <GameCardSkeleton key={s} />)}
-        {data.map((game) => (
-          <GameCard game={game} key={game.id} />
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{
+        sm: 1,
+        md: 2,
+        lg: 3,
+        xl: 4,
+      }}
+      spacing={5}
+      padding={{
+        sm: 2,
+        md: 4,
+      }}
+      paddingLeft={{
+        lg: 0,
+      }}
+      justifyContent="center"
+    >
+      {isLoading && skeletons.map((s) => <GameCardSkeleton key={s} />)}
+      {data.map((game) => (
+        <GameCard game={game} key={game.id} />
+      ))}
+    </SimpleGrid>
   );
 };
 
