db2074c4e02874ff5b85497b569a88860f5f93b2
phyothiha-aung <phyothiha050@gmail.com>
2023-05-04 22:30:12 +0630

    refector: simply time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse, Platform } from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("games");
 
@@ -29,7 +30,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPage) => {
       return lastPage.next ? allPage.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
   });
 
 export default useGames;
