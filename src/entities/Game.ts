1e4bcc30cf43e275a512a63fc6932d7dc412076c
phyothiha-aung <phyothiha050@gmail.com>
2023-05-07 09:56:50 +0630

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
+  publishers: Publisher[];
   metacritic: number;
   rating_top: number;
   slug: string;
