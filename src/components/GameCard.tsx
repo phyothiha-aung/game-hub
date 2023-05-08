f4749293d3d9568a9d1ee436778bb3a91318efce
phyothiha-aung <phyothiha050@gmail.com>
2023-05-08 07:28:12 +0630

    clickable card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,25 +10,25 @@ interface GameCardProps {
 }
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card
-      borderRadius={10}
-      overflow="hidden"
-      _hover={{ transform: "scale(1.03)", transition: "all 0.15s ease-in" }}
-    >
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={1}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Rating rating={game.rating_top} />
-        <Heading fontSize="2xl">
-          <Link to={`/games/${game.slug}`}>{game.name}</Link>
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={`/games/${game.slug}`}>
+      <Card
+        borderRadius={10}
+        overflow="hidden"
+        _hover={{ transform: "scale(1.03)", transition: "all 0.15s ease-in" }}
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={1}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Rating rating={game.rating_top} />
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
